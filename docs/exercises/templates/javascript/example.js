184c18dc43d39d0066f7e2761f63412c2c6716f7
Maximilian Hallqvist <98323670+NoLifer47@users.noreply.github.com>
2022-05-16 19:01:14 +0200

    first commit

---
+++ docs/exercises/templates/javascript/example.js
@@ -0,0 +1,12 @@
+function visaDialog() {
+    alert('Hej på dej!');
+    changeText();
+}
+
+function changeText() {}
+document.getElementById("demo").innerHTML = "Hello  javascript;";
+
+function visaDialog() {
+    alert('hehe noob');
+    changeText();
+}
